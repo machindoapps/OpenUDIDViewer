700711024d5065007bc9b93e15f62ebf4df578c7
David Haynes <machindoapps@gmail.com>
2013-02-26 11:33:43 +0000

    Initial Commit

---
+++ OpenUDIDViewer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OpenUDIDViewer
+//
+//  Created by David Haynes on 26/02/2013.
+//  Copyright (c) 2013 Machindo Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OpenUDIDViewer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  OpenUDIDViewer
+//
+//  Created by David Haynes on 26/02/2013.
+//  Copyright (c) 2013 Machindo Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
